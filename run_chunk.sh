861b7a333a69da3fb6f48e54b3533857e8295206
Thiago Hersan <thiago.hersan+github@gmail.com>
2024-05-29 20:08:25 -0300

    iupdate run_chunk

--- run_chunk.sh
+++ run_chunk.sh
@@ -4,6 +4,6 @@ if [ $# -eq 0 ]; then
     echo "No data directory supplied"
 else
     for filename in "$1"/*.wav; do
-        python chunk_audio.py --input_file "$filename" --output_dir "$1"/chunks --chunk_length 2000 --overlap 1000
+        python chunk_audio.py --input_file "$filename" --output_dir "$1"/chunks --chunk_length 5000 --overlap 4000
     done
 fi
